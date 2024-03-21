f34d44a707998e14eebe22286ebfc7119a8cf755
Francisco Cuevas <fmcuevas1985@outlook.com>
2024-03-20 21:50:59 -0300

    crear los metodos para actualizar productos en el controller

--- ecommerce/src/main/java/py/com/curso/ecommerce/controller/ProductoController.java
+++ ecommerce/src/main/java/py/com/curso/ecommerce/controller/ProductoController.java
@@ -5,11 +5,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import py.com.curso.ecommerce.model.Producto;
 import py.com.curso.ecommerce.model.Usuario;
 import py.com.curso.ecommerce.service.ProductoService;
+
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/productos")
@@ -38,6 +41,24 @@ public class ProductoController {
         producto.setUsuario(usuario);
 
         service.save(producto);
+        return "redirect:/productos";
+    }
+
+    @GetMapping("/edit/{id}")
+    public String edit(@PathVariable Long id, Model model) {
+        Producto producto = new Producto();
+        Optional<Producto> optionalProducto = service.get(id);
+        producto = optionalProducto.get();
+        log.info("Producto buscado: {}", producto.getId());
+
+        model.addAttribute("producto", producto);
+        //service.save(producto);
+        return "/productos/edit";
+    }
+
+    @PostMapping("/update")
+    public String update(Producto producto) {
+        service.update(producto);
         return "redirect:/productos";
     }
 
