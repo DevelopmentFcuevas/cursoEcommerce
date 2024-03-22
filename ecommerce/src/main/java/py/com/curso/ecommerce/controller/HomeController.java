0ab88647d60c31519a081126824aa1edcf0de5ea
Francisco Cuevas <fmcuevas1985@outlook.com>
2024-03-21 21:41:04 -0300

    mostrar la vista del producto al usuario

--- ecommerce/src/main/java/py/com/curso/ecommerce/controller/HomeController.java
+++ ecommerce/src/main/java/py/com/curso/ecommerce/controller/HomeController.java
@@ -1,14 +1,17 @@
 package py.com.curso.ecommerce.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import py.com.curso.ecommerce.service.ProductoService;
 
 @Controller
 @RequestMapping("/")
+@Slf4j
 public class HomeController {
     @Autowired
     private ProductoService productoService;
@@ -18,4 +21,11 @@ public class HomeController {
         model.addAttribute("productos", productoService.findAll());
         return "usuario/home";
     }
+
+    @GetMapping("productohome/{id}")
+    public String productHome(@PathVariable Long id) {
+        log.info("Id enviado como parametro: {}", id);
+        return "usuario/productohome";
+    }
+
 }
