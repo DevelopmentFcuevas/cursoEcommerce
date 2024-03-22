60e49da9fe619540aaa3637f9c332f85cafeb936
Francisco Cuevas <fmcuevas1985@outlook.com>
2024-03-22 09:11:41 -0300

    redireccion al carrito de compras

--- ecommerce/src/main/java/py/com/curso/ecommerce/controller/HomeController.java
+++ ecommerce/src/main/java/py/com/curso/ecommerce/controller/HomeController.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import py.com.curso.ecommerce.model.Producto;
 import py.com.curso.ecommerce.service.ProductoService;
@@ -33,6 +34,11 @@ public class HomeController {
         producto = productoOptional.get();
         model.addAttribute("producto", producto);
         return "usuario/productohome";
+    }
+
+    @PostMapping("/cart")
+    public String addCart() {
+        return "usuario/carrito";
     }
 
 }
