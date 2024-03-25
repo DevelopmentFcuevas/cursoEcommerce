ecc11ff4aecbb63acd2187a80c40a6e7ff30bbcd
Francisco Cuevas <fmcuevas1985@outlook.com>
2024-03-25 16:09:48 -0400

    mostrar la vista compras para el user

--- ecommerce/src/main/java/py/com/curso/ecommerce/controller/UsuarioController.java
+++ ecommerce/src/main/java/py/com/curso/ecommerce/controller/UsuarioController.java
@@ -4,6 +4,7 @@ import jakarta.servlet.http.HttpSession;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -61,6 +62,12 @@ public class UsuarioController {
         }
 
         return "redirect:/";
+    }
+
+    @GetMapping("/compras")
+    public String obtenerCompras(Model model, HttpSession session) {
+        model.addAttribute("session", session.getAttribute("idUsuario"));
+        return "usuario/compras";
     }
 
 }
