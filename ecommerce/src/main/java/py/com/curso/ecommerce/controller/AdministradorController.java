b74e374c58bf46358dba0df268dc3d9266964077
Francisco Cuevas <fmcuevas1985@outlook.com>
2024-03-26 12:20:52 -0400

    Funcionalidad para ver las ordenes por el admin

--- ecommerce/src/main/java/py/com/curso/ecommerce/controller/AdministradorController.java
+++ ecommerce/src/main/java/py/com/curso/ecommerce/controller/AdministradorController.java
@@ -6,6 +6,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import py.com.curso.ecommerce.model.Producto;
+import py.com.curso.ecommerce.service.OrdenService;
 import py.com.curso.ecommerce.service.ProductoService;
 import py.com.curso.ecommerce.service.UsuarioService;
 
@@ -20,6 +21,9 @@ public class AdministradorController {
     @Autowired
     private UsuarioService usuarioService;
 
+    @Autowired
+    private OrdenService ordenService;
+
     @GetMapping("")
     public String home(Model model) {
         List<Producto> productos = productoService.findAll();
@@ -31,6 +35,12 @@ public class AdministradorController {
     public String usuarios(Model model) {
         model.addAttribute("usuarios", usuarioService.findAll());
         return "administrador/usuarios";
+    }
+
+    @GetMapping("/ordenes")
+    public String ordenes(Model model) {
+        model.addAttribute("ordenes", ordenService.findAll());
+        return "administrador/ordenes";
     }
 
 }
